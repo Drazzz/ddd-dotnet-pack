ca7323ab94bd57e74ee83faaeb44c399b067119e
Drazzz <pawel.kulas@pl.abb.com>
2021-05-21 07:18:55 +0200

    add base project structure

---
+++ src/Domain/ddd-dotnet-pack.Domain.Abstractions/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ddd_dotnet_pack.Domain.Abstractions.Exceptions
+{
+    public abstract class DomainException : ApplicationException
+    {
+        protected DomainException() { }
+        protected DomainException(string message) : base(message) { }
+        protected DomainException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
